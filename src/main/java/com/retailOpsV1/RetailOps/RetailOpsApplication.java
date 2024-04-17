45fd3485010db5696f6d1da8c7e82c32fb707716
AbelG101 <abelgebeyehu101@gmail.com>
2024-04-17 18:52:09 +0300

    Add init files

---
+++ src/main/java/com/retailOpsV1/RetailOps/RetailOpsApplication.java
@@ -0,0 +1,13 @@
+package com.retailOpsV1.RetailOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailOpsApplication.class, args);
+	}
+
+}
