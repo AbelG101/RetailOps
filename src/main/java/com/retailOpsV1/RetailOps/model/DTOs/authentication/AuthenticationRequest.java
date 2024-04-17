2f4a440795c118391baad7e97e3d9f0aa7308cd5
AbelG101 <abelgebeyehu101@gmail.com>
2024-04-17 18:59:31 +0300

    Add authentication Request and Response DTO model classes

---
+++ src/main/java/com/retailOpsV1/RetailOps/model/DTOs/authentication/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.retailOpsV1.RetailOps.model.DTOs.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String userName;
+    private String password;
+}
---
+++ src/main/java/com/retailOpsV1/RetailOps/model/DTOs/authentication/AuthenticationResponse.java
@@ -0,0 +1,21 @@
+package com.retailOpsV1.RetailOps.model.DTOs.authentication;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    private Integer employeeId;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String roleName;
+}
